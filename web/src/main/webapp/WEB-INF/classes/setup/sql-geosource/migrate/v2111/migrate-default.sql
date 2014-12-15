051f28e64969cc1f2257d46e6c976e2ed4de8043
François Prunayre <fxprunayre@gmail.com>
2014-12-15 15:59:22 +0100

    Add new setting.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/v2111/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/v2111/migrate-default.sql
@@ -1,1 +1,3 @@
 UPDATE Settings SET value='2.11.1' WHERE name='system/platform/version';
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/resourceIdentifierPrefix', 'http://localhost:8080/geosource/', 0, 10001, 'n');
