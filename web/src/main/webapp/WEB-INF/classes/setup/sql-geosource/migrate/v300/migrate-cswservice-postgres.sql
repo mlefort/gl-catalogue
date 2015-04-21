beb9e1598c8ac5436c2afb101555ec1347355eb7
François Prunayre <fxprunayre@gmail.com>
2015-04-21 11:48:36 +0200

    Update DB migration.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/v300/migrate-cswservice-postgres.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/v300/migrate-cswservice-postgres.sql
@@ -3,6 +3,10 @@ UPDATE ServiceParameters SET occur='+';
 
 create sequence serviceparameter_id_seq start with 1 increment by 1;
 alter table serviceparameters add column id integer;
-UPDATE serviceparameters SET ID=nextval('serviceparameter_id_seq')
+UPDATE serviceparameters SET ID=nextval('serviceparameter_id_seq');
 
-ALTER TABLE SERVICEPARAMETERS ADD PRIMARY KEY (id)
+ALTER TABLE ServiceParameters DROP CONSTRAINT IF EXISTS serviceparameters_service_fkey;
+ALTER TABLE ServiceParameters DROP CONSTRAINT IF EXISTS serviceparameters_pkey;
+ALTER TABLE SERVICEPARAMETERS ADD PRIMARY KEY (id);
+
+ALTER TABLE Schematron RENAME file TO filename;
--- web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/v300/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/v300/migrate-default.sql
@@ -1,5 +1,3 @@
-UPDATE Settings SET value='3.0.0' WHERE name = 'system/platform/version';
-
 INSERT INTO settings (name, value, datatype, position, internal) VALUES ('system/server/log','log4j.xml',0,250,'y');
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('region/getmap/background', 'osm', 0, 9590, 'n');
@@ -21,3 +19,5 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 UPDATE Settings SET value = '{"viewerMap": "../../map/config-viewer.xml", "listOfServices": {"wms": [], "wmts": []}, "useOSM":true,"context":"","layer":{"url":"http://www2.demis.nl/mapserver/wms.asp?","layers":"Countries","version":"1.1.1"},"projection":"EPSG:3857","projectionList":[{"code":"EPSG:4326","label":"WGS84 (EPSG:4326)"},{"code":"EPSG:3857","label":"Google mercator (EPSG:3857)"}]}' WHERE name = 'map/config';
 
 UPDATE Settings SET value = '[{"code":"EPSG:2154","value":"+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"}]' WHERE name = 'map/proj4js';
+
+UPDATE Settings SET value='3.0.0' WHERE name = 'system/platform/version';
