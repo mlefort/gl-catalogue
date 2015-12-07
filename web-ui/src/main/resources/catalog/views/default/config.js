21512852974d125260f7e147b509734fd97c7d39
François Prunayre <fx.prunayre@gmail.com>
2015-12-07 16:25:40 +0100

    Improve grandlyon view. Backport some of GN3.0.3 improvements.

--- web-ui/src/main/resources/catalog/views/default/config.js
+++ web-ui/src/main/resources/catalog/views/default/config.js
@@ -147,9 +147,9 @@
             //  label: 'inspire',
             //  url: 'md.format.xml?xsl=xsl-view' + '&view=inspire&id='
             //}, {
-            //  label: 'full',
-            //  url: 'md.format.xml?xsl=xsl-view&view=advanced&id='
-            //}, {
+              label: 'grandlyon',
+              url: 'md.format.xml?xsl=xsl-view&view=grandlyon&uuid='
+            }, {
               label: 'full',
               url: 'md.format.xml?xsl=full_view&uuid='
               /*
