c641b909905cf09b6287323701b0f66cbe0e3de5
Matthieu Lefort <m.lefort@neogeo-online.net>
2016-09-28 12:02:12 +0200

    Report fix FXP pour suppression user via API
    Fix FXP original : https://github.com/geonetwork/core-geonetwork/commit/b0580479ab6a16fe3d9f9ed90a73360b34e1fcdc

--- services/src/main/java/org/fao/geonet/services/user/Remove.java
+++ services/src/main/java/org/fao/geonet/services/user/Remove.java
@@ -36,6 +36,7 @@ import org.fao.geonet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -79,6 +80,16 @@ public class Remove {
             UserSession usrSess = (UserSession) tmp;
             myProfile = usrSess.getProfile();
             myUserId = usrSess.getUserId();
+        } else if (tmp == null) {
+            Object securityContext = session.getAttribute("SPRING_SECURITY_CONTEXT");
+            if (securityContext instanceof SecurityContext) {
+                Object principal = ((SecurityContext)securityContext).getAuthentication().getPrincipal();
+                if (principal instanceof User) {
+                    User user = (User)principal;
+                    myProfile = user.getProfile();  
+                    myUserId = user.getId() + "";
+                }
+            }
         }
 
         if (myUserId == null || myUserId.equals(id)) {
